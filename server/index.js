f957f5d2cc51e9848111b946b31a882a82303cff
Aleksandr Makarov <amk-ext@marketlogicsoftware.com>
2021-03-01 16:27:41 +0300

    init

---
+++ server/index.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+const express = require('express');
+
+const PORT = process.env.PORT || 7000;
+
+const app = express();
+
+
+app.listen(PORT, ()=> console.log(`Server listening on port ${PORT}`));
