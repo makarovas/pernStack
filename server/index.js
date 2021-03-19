3adfe5a75c4536dfe013a142412517d1f9222586
Aleksandr Makarov <amk-ext@marketlogicsoftware.com>
2021-03-19 21:50:19 +0300

    added db models

--- server/index.js
+++ server/index.js
@@ -1,10 +1,20 @@
 require('dotenv').config()
 const express = require('express');
 const sequelize = require('./db');
+const models = require('./models/models.js')
+const cors = require('cors')
+
 
 const PORT = process.env.PORT || 7000;
 
 const app = express();
+app.use(cors());
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.status(200).json({message: 'WORKING'})
+  
+} )
 
 const start = async () => {
   try {
